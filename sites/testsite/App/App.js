ff41248467edd2c3bd2caffe1eeb34149ee90556
Georgios Dimitriadis CH <gedi01@WL000507.ad.comhem.com>
2019-07-31 13:56:32 +0200

    testsite: Test counter as react hook

--- sites/testsite/App/App.js
+++ sites/testsite/App/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import styles from './App.less';
 import HeartIcon from '../resources/icons/heart.svg';
+import Counter from '../Counter/Counter';
 
 const App = () => {
     return (
         <div className={styles.app}>
             Hello World from React
             <img className={styles.icon} src={HeartIcon} />
+            <br />
+            <Counter />
         </div>
     );
 };
---
+++ sites/testsite/Counter/Counter.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import styles from './Counter.less';
+
+const Counter = () => {
+    const [currentCount, setCurrentCount] = useState(0);
+    return (
+        <div className={styles.counter}>
+            Current count: {currentCount}
+            <br/>
+            <button onClick={() => setCurrentCount(currentCount-1)}>-</button>
+            <button onClick={() => setCurrentCount(currentCount+1)}>+</button>
+        </div>
+    );
+};
+
+export default Counter;
